e56bd39c8e2fd7a0714ba1590d6662e86a045932
Wai Yan Min Htet <waiyanminhtetmdy786@gmail.com>
2024-04-06 21:15:38 +0630

    add deployment script

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import path, { dirname } from "node:path"
 
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/users.routes.js";
@@ -10,6 +11,7 @@ import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 server.listen(PORT, () => {
     mongoose.connect(process.env.MONGODB_URI).then(() => console.log('connect to mongodb')).catch(error => console.log(error));
@@ -23,6 +25,11 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/message', messageRoutes);
+
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+});
 
 app.use((err, req, res, next) => {
     const errorStatus = err.statusCode || 500;
